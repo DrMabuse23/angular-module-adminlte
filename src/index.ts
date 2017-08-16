b02c80dd480ac0c7b5c03b959161474dd07788f0
Jose Chero Sojo <jchero.sojo@gmail.com>
2017-08-16 18:36:55 -0500

    Coding time!

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,57 @@
-export { SeedModule } from './seed.module';
-export { SeedService } from './seed.service';
-export { SeedComponent } from './seed.component';
+
+// services
+export { BreadcrumbService } from './services/breadcrumb.service';
+//export * from './services/breadcrumb.service';
+
+export { CanActivateGuard } from './services/can-activate-guard.service';
+//export * from './services/can-activate-guard.service';
+
+export { LoggerService } from './services/logger.service';
+//export * from './services/logger.service';
+
+export { MessagesService } from './services/messages.service';
+//export * from './services/messages.service';
+
+export { NotificationsService } from './services/notifications.service';
+//export * from './services/notifications.service';
+
+export { RestService } from './services/rest.service';
+//export * from './services/rest.service';
+
+export { TranslateService } from './services/translate.service';
+//export * from './services/translate.service';
+
+export { UserService } from './services/user.service';
+//export * from './services/user.service';
+
+export { MenuService } from './services/menu.service';
+//export * from './services/menu.service';
+
+export{ LogoService } from './services/logo.service';
+//export * from './services/logo.service';
+
+// layouts
+export { LayoutAuthComponent } from './layouts/auth/auth';
+//export * from './layouts/auth/auth';
+
+export { LayoutLoginComponent } from './layouts/login/login.component';
+//export * from './layouts/login/login.component';
+
+export { LayoutRegisterComponent } from './layouts/register/register.component';
+//export * from './layouts/register/register.component';
+
+// models
+export { Message } from './models/message';
+//export * from './models/message';
+
+export { Preferencies } from './models/preferencies';
+//export * from './models/preferencies';
+
+export { User } from './models/user';
+//export * from './models/user';
+
+export { Notification } from './models/notification';
+//export * from './models/notification';
+
+export { AdminLteModule } from './adminlte.module';
+//export { AdminLteModule } from './adminlte.module';
