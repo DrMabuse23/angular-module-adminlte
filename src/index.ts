ac10f9f8bd6b4abad66c98df1b12a30ce9395649
Jose Chero Sojo <jchero.sojo@gmail.com>
2017-08-21 15:00:11 -0500

    Config test

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,57 @@
 
 // services
-export { BreadcrumbService } from './services/breadcrumb.service';
-//export * from './services/breadcrumb.service';
+import { BreadcrumbService } from './services/breadcrumb.service';
+export * from './services/breadcrumb.service';
 
-export { CanActivateGuard } from './services/can-activate-guard.service';
-//export * from './services/can-activate-guard.service';
+import { CanActivateGuard } from './services/can-activate-guard.service';
+export * from './services/can-activate-guard.service';
 
-export { LoggerService } from './services/logger.service';
-//export * from './services/logger.service';
+import { LoggerService } from './services/logger.service';
+export * from './services/logger.service';
 
-export { MessagesService } from './services/messages.service';
-//export * from './services/messages.service';
+import { MessagesService } from './services/messages.service';
+export * from './services/messages.service';
 
-export { NotificationsService } from './services/notifications.service';
-//export * from './services/notifications.service';
+import { NotificationsService } from './services/notifications.service';
+export * from './services/notifications.service';
 
-export { RestService } from './services/rest.service';
-//export * from './services/rest.service';
+import { RestService } from './services/rest.service';
+export * from './services/rest.service';
 
-export { TranslateService } from './services/translate.service';
-//export * from './services/translate.service';
+import { TranslateService } from './services/translate.service';
+export * from './services/translate.service';
 
-export { UserService } from './services/user.service';
-//export * from './services/user.service';
+import { UserService } from './services/user.service';
+export * from './services/user.service';
 
-export { MenuService } from './services/menu.service';
-//export * from './services/menu.service';
+import { MenuService } from './services/menu.service';
+export * from './services/menu.service';
 
-export{ LogoService } from './services/logo.service';
-//export * from './services/logo.service';
+import{ LogoService } from './services/logo.service';
+export * from './services/logo.service';
 
 // layouts
-export { LayoutAuthComponent } from './layouts/auth/auth';
-//export * from './layouts/auth/auth';
+import { LayoutAuthComponent } from './layouts/auth/auth';
+export * from './layouts/auth/auth';
 
-export { LayoutLoginComponent } from './layouts/login/login.component';
-//export * from './layouts/login/login.component';
+import { LayoutLoginComponent } from './layouts/login/login.component';
+export * from './layouts/login/login.component';
 
-export { LayoutRegisterComponent } from './layouts/register/register.component';
-//export * from './layouts/register/register.component';
+import { LayoutRegisterComponent } from './layouts/register/register.component';
+export * from './layouts/register/register.component';
 
 // models
-export { Message } from './models/message';
-//export * from './models/message';
+import { Message } from './models/message';
+export * from './models/message';
 
-export { Preferencies } from './models/preferencies';
-//export * from './models/preferencies';
+import { Preferencies } from './models/preferencies';
+export * from './models/preferencies';
 
-export { User } from './models/user';
-//export * from './models/user';
+import { User } from './models/user';
+export * from './models/user';
 
-export { Notification } from './models/notification';
-//export * from './models/notification';
+import { Notification } from './models/notification';
+export * from './models/notification';
 
+import { AdminLteModule } from './adminlte.module';
 export { AdminLteModule } from './adminlte.module';
-//export { AdminLteModule } from './adminlte.module';
